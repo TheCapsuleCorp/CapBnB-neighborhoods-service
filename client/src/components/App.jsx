45660601e502492849fe9988742f79e2f4adf320
John Von Schlichtn <vonschlichten@Johns-MacBook-Air.local>
2019-11-19 22:48:38 +0700

    app.jsx update for branch switch

---
+++ client/src/components/App.jsx
@@ -0,0 +1,40 @@
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+//   handleClick() {
+//     console.log('click');
+//   }
+//   render() {
+//     return (
+//
+//     );
+//   }
+// }
+// export default App;
---
+++ client/src/index.jsx
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/App.jsx';
+//
+//
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
