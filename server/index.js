13cfdeb9c414d001987301428bf8b29c5011e765
John Von Schlichtn <vonschlichten@Johns-MacBook-Air.local>
2019-11-22 12:10:08 +0700

    Server Update, Package.json Update

--- server/index.js
+++ server/index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const parser = require('body-parser');
+const bodyparser = require('body-parser');
+const path = require('path');
+const mongodo = require('../database');
+// const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
 
-app.use(parser.json());
-// use body-parser to extract the entire body portion of an incoming request stream and expose it in req.body
+
+app.use(bodyparser.json());
+//looks at requests where the Content-Type: application/json header is present
+//and transforms the text-based JSON input into JS-accessible variables under req.body.
 app.use(express.static('.client/dist'));
+// app.use(express.static(`${__dirname}/../client/dist`));
 //to serve static files such as images, CSS files, and JavaScript files
+app.use(bodyparser.urlencoded({ extended: true }));
+//app.use(bp.urlencoded({extended: true}) works similarly to body parse the extended: true
+//ensures that the req.body object will contain values of any type instead of just strings.
 
-app.get('/', (req, res) => {
+
+app.get('/neighborhoods', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+app.listen(PORT, () => {
+  console.log(`Connection Success! Listening on port ${PORT}!`);
 });
