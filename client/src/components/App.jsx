ef6e57079a11602b4ce718c7683d4f80ed89c229
John Von Schlichtn <vonschlichten@Johns-MacBook-Air.local>
2019-11-29 17:46:54 +0700

    Refactor App component. Details component renders properly. Code Review changes made.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -16,17 +16,12 @@ class App extends React.Component {
 
   componentDidMount() {
 
-
-
   };
 
   render() {
     return (
       <div className="App">
         <h2>The neighborhood</h2>
-        <p className="description">{this.state.name}'s place is located in {this.state.city}, {this.state.country}.</p>
-        <p>{this.state.description}</p>
-        <p>{this.state.gettingAround}</p>
         <Details guidbookId={this.state.guidebookId} />
       </div>
     );
