140112837d624cfe27e3fc08ac0ef07b91367209
John Von Schlichtn <vonschlichten@Johns-MacBook-Air.local>
2019-11-29 11:20:02 +0700

    App component shell update. Ready for refactor upon db connection.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
+import Details from './Details.jsx';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      hostName: '',
+      city: '',
+      country: '',
+      gettingAround: '',
+      description: '',
+      guidebookId: null
+    };
   }
+
+  componentDidMount() {
+
+    // $.ajax({
+    //   url: '/api/rooms/11111111/neighborhood',
+    //   dataType: 'json',
+    //   method: 'GET',
+    //   contentType: 'application/json',
+    //   // data: JSON.stringify({jsonData: data}),
+    //   success: function(data) {
+    //     console.log(data);
+    //   },
+    //   error: function(error) {
+    //     console.log(error);
+    //   },
+    //
+    // });
+
+  };
 
   render() {
     return (
-      <div className="helloworld">Hello World!</div>
+      <div className="App">
+        <h2>The neighborhood</h2>
+        <p className="description">{this.state.name}'s place is located in {this.state.city}, {this.state.country}.</p>
+        <p>{this.state.description}</p>
+        <p>{this.state.gettingAround}</p>
+        <Details guidbookId={this.state.guidebookId} />
+      </div>
     );
   }
 }
