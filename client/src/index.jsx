3a5fbe755099df65e1dee2c2e8e692429db79909
John Von Schlichtn <vonschlichten@Johns-MacBook-Air.local>
2019-12-05 22:05:55 +0700

    Created roomId variable as a prop to dynamically use different data from different room ids.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,4 +4,4 @@ import App from './components/App';
 
 
 
-ReactDOM.render(<App roomId={11111111} />, document.getElementById('root'));
+ReactDOM.render(<App roomId={11111112} />, document.getElementById('root'));
