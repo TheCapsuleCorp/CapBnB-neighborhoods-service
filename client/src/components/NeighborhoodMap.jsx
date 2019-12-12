74f24c2939176efe258c3ad4dfbec805f9773e0b
John Von Schlichtn <jpvonschlichten@gmail.com>
2019-12-12 12:07:29 +0700

    Import css module, update googleMap styles to use grid.

--- client/src/components/NeighborhoodMap.jsx
+++ client/src/components/NeighborhoodMap.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Details from './Details';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
-
+import '../../dist/index.css';
 // DO NOT COMMIT ME TO VERSION CONTROL!!!!!
 const GOOGLE_MAPS_API_KEY = 'AIzaSyALnk5uTIhLwMzcanWu3Dspzuh3Gt47C2k';
 
@@ -12,26 +12,24 @@ class NeighborHoodMap extends React.Component {
 
   render() {
     const mapStyles = {
-      width: '100%',
+      gridRow: 1/2,
+      width: '58vw',
       height: '500px',
-      position: 'relative',
+      // position: 'relative',
     };
 
     const { lat, lng } = this.props;
 
     return (
-    <div>
       <div className="map-wrapper">
-        <div className="map-out">
-          <div className="map-in">
-            <Map
-              google={this.props.google}
-              zoom={8}
-              style={mapStyles}
-              initialCenter={{ lat: lat, lng: lng}}
-            />
-            </div>
-          </div>
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: lat, lng: lng}}
+        />
+        <div className="exact-location">
+          Exact location information is provided after a booking is confirmed.
         </div>
       </div>
     );
@@ -41,3 +39,9 @@ class NeighborHoodMap extends React.Component {
 export default GoogleApiWrapper({
   apiKey: GOOGLE_MAPS_API_KEY,
 })(NeighborHoodMap);
+
+
+// <div className="map-out">
+//   <div className="map-in">
+//   </div>
+// </div>
